f6968f0c11faf29d8523a2e884f7c380f84de6b6
KristineGray <krisgraya@gmail.com>
2021-07-23 12:03:29 -0400

    created action method SubmitAddUserForm and added if/else for password validation

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,27 @@ namespace SpaDay.Controllers
         public IActionResult Add()
         {
             return View();
+        }
+
+        // Create action method for SubmitAddUserForm
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+
+            // Check that the verify parameter matches the password within the newUser object
+            // If true: 
+                // store the user’s name in a ViewBag property and
+                // return View("Index").
+            // If false, render the form again
+            if (newUser.Password == verify)
+            {
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                return View("Add");
+            }
         }
     }
 }
