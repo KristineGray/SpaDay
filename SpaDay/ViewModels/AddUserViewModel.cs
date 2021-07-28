29d7ce05115848770c38ad7b64c03422cfba7c8a
KristineGray <krisgraya@gmail.com>
2021-07-28 14:15:32 -0400

    added Required validation attribute to Username, Password, and VerifyPassword in AddUserViewModel.cs

--- SpaDay/ViewModels/AddUserViewModel.cs
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace SpaDay.ViewModels
 {
     public class AddUserViewModel
     {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
         public string Password { get; set; }
+        
         public string Email { get; set; }
+        
+        [Required]
         public string VerifyPassword { get; set; }
     }
 }
