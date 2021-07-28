dcccaec8bde6dc3f19cff8711315dfc2ff1884a2
KristineGray <krisgraya@gmail.com>
2021-07-28 14:20:49 -0400

    added error messages to AddUserViewModel.cs props

--- SpaDay/ViewModels/AddUserViewModel.cs
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -8,16 +8,16 @@ namespace SpaDay.ViewModels
 {
     public class AddUserViewModel
     {
-        [Required]
-        [StringLength(15, MinimumLength = 5)]
+        [Required(ErrorMessage ="Username Required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage ="Username must be between 5 and 15 characters long")]
         public string Username { get; set; }
         
-        [Required]
-        [StringLength(20, MinimumLength = 6)]
+        [Required(ErrorMessage = "Password Required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters long")]
         public string Password { get; set; }
         
-        [EmailAddress]
-        [StringLength(20, MinimumLength = 6)]
+        [EmailAddress(ErrorMessage = "Verify Password Required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Verify Password must be between 6 and 20 characters long")]
         public string Email { get; set; }
         
         [Required]
